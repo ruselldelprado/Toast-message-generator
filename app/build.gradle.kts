9f3695fbea77ed8944b6d645a57d2e528d41b32b
Rusell Del Prado <rusellvdelprado@gmail.com>
2024-08-01 02:08:33 +0800

    Added component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "dev.rusell.toast_generator"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "dev.rusell.toast_generator"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/dev/rusell/toast_generator/MainActivity.kt
+++ app/src/main/java/dev/rusell/toast_generator/MainActivity.kt
@@ -1,15 +1,30 @@
 package dev.rusell.toast_generator
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.rusell.toast_generator.ui.theme.ToastgeneratorTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +37,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Generator()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Generator() {
+    var text by remember { mutableStateOf("") }
+    val context = LocalContext.current
+    Column (
+        modifier = Modifier.fillMaxWidth(1f),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Spacer(modifier = Modifier.height(40.dp))
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Type your toast message") }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = {
+            if(text != "") {
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+                text = ""
+            }
+        }) {
+            Text(text = "Click to generate toast")
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun GeneratorPreview() {
+    Generator()
+}
